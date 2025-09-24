cd0e55f6b25945fd2151d132530a6b3c2085a9f1
Meng Thao <mthaoqk@gmail.com>
2025-09-23 21:27:07 -0500

    9.23.25

--- taskManager/src/App.tsx
+++ taskManager/src/App.tsx
@@ -7,17 +7,28 @@ interface Task {
   completed: boolean;
 }
 
-const initialTasks = [{ id: 1, text: "The World is Yours", completed: false }, {id: 2, text: "We are getting there, now add tasks", completed: false}];
+interface Digimon {
+  id: number;
+  name: string;
+  type: string;
+}
 
+const initialTasks = [
+  { id: 1, text: "The World is Yours", completed: false },
+  { id: 2, text: "We are getting there, now add tasks", completed: false },
+];
+const starterMon = [{ id: 0, name: "WarGreymon", type: "Vaccine" }];
 function App() {
   const [tasks, setTasks] = useState<Task[]>(initialTasks);
+  const [digiMon, setdigiMon] = useState<Digimon[]>(starterMon);
+  const [newTask,setNewTask] = useState<string>("");
 
   return (
     <>
       <h1>Task Manager</h1>
       <div>
         <h2>Tasks</h2>
-        <ul>
+        <ul id="taskList">
           {tasks.map((task) => (
             <li key={task.id}>
               {task.text}{" "}
@@ -29,12 +40,30 @@ function App() {
                     tasks.map((t) =>
                       t.id === task.id ? { ...t, completed: !t.completed } : t
                     )
-                  )
+                  );
                 }}
               />
               <span
                 className={task.completed ? "completed-task" : "task-text"}
               ></span>
+            </li>
+          ))}
+        </ul>
+        <form action="" onSubmit={(e) => e.preventDefault()
+
+        }>
+          <input type="text" placeholder="Add a task" value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
+          <button>Submit</button>
+        </form>
+      </div>
+
+      <div id="digimon">
+        <ul>
+          {digiMon?.map((mon) => (
+            <li key={mon.id}>
+              <p>
+                Your digimon is {mon.name} and it's a {mon.type} type!
+              </p>
             </li>
           ))}
         </ul>
